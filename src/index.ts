0399cfd48269409f150ffefb5bd993c2c598b5fe
Kenneth Syharath <kenneth.syharath@gmail.com>
2021-12-31 16:32:55 -0500

    Getting in all missing exports
    Missed some basic exports; added Action, OutcomeRuleset, and DnDOutcomeRuleset

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import { Roll } from './models/Roll';
 import { Result } from './models/Result';
+import { Action } from './models/Action';
+import { OutcomeRuleset } from './models/OutcomeRuleset';
+import { DndOutcomeRuleset } from './models/DndOutcomeRuleset';
 
-export { Roll, Result };
+export { Roll, Result, Action, OutcomeRuleset, DndOutcomeRuleset };

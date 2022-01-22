a68f4c2115e15fdc737aef717058843e777ea02a
Kenneth Syharath <kenneth.syharath@gmail.com>
2022-01-22 14:07:19 -0500

    Edits for npm publish
    swapping var for const mostly

--- src/__tests/Character.test.ts
+++ src/__tests/Character.test.ts
@@ -1,88 +1,59 @@
-import { Action, DndOutcomeRuleset, Roll } from "..";
-import { Character } from "../models/Character";
+import { Action, DndOutcomeRuleset, Roll } from '..';
+import { Character } from '../models/Character';
 
 test('can assign stat', () => {
-  const char = new Character(
-    [
-      ["strength", 10]
-    ], []
-  );
-  
-  expect(char.getStat("strength")).toEqual(10);
+  const char = new Character([['strength', 10]], []);
+
+  expect(char.getStat('strength')).toEqual(10);
 });
 
 test('get unknown stat is undefined', () => {
-  const char = new Character(
-    [
-      ["strength", 10]
-    ],
-    []
-  );
+  const char = new Character([['strength', 10]], []);
 
-  expect(char.getStat("dexterity")).toBeUndefined();
+  expect(char.getStat('dexterity')).toBeUndefined();
 });
 
 test('can update stat value', () => {
-  const char = new Character(
-    [
-      ["strength", 10]
-    ],
-    []
-  );
+  const char = new Character([['strength', 10]], []);
 
-  char.setStat("strength", 13);
+  char.setStat('strength', 13);
 
-  expect(char.getStat("strength")).toEqual(13);
+  expect(char.getStat('strength')).toEqual(13);
 });
 
 test('updating an undeclared stat fails', () => {
   const char = new Character([], []);
 
-  char.setStat("strength", 13);
+  char.setStat('strength', 13);
 
-  expect(char.getStat("strength")).toBeUndefined();
+  expect(char.getStat('strength')).toBeUndefined();
 });
 
 test('can override default behavior of updating undeclared stat', () => {
   const char = new Character([], []);
-  
-  char.setStat("strength", 13, true);
 
-  expect(char.getStat("strength")).toEqual(13);
+  char.setStat('strength', 13, true);
+
+  expect(char.getStat('strength')).toEqual(13);
 });
 
 test('can assign actions', () => {
-  const action = new Action([
-    [
-      'toHit', 
-      new Roll(0, new DndOutcomeRuleset(), 20)
-    ],
-  ])
-  
-  const char = new Character([], [
-    ["attack", action]
-  ]);
+  const action = new Action([['toHit', new Roll(0, new DndOutcomeRuleset(), 20)]]);
 
-  expect(char.actions.has("attack")).toBeTruthy();
-  expect(char.actions.get("attack")).toEqual(action);
+  const char = new Character([], [['attack', action]]);
+
+  expect(char.actions.has('attack')).toBeTruthy();
+  expect(char.actions.get('attack')).toEqual(action);
 });
 
 test('can perform given action', () => {
   const action = new Action([
-    [
-      'toHit', 
-      new Roll(0, new DndOutcomeRuleset(), 20)
-    ],
-    [
-      'damage',
-      new Roll(0, new DndOutcomeRuleset(), 8)
-    ]
-  ])
-  
-  const char = new Character([], [
-    ["attack", action]
+    ['toHit', new Roll(0, new DndOutcomeRuleset(), 20)],
+    ['damage', new Roll(0, new DndOutcomeRuleset(), 8)],
   ]);
-  
+
+  const char = new Character([], [['attack', action]]);
+
   const results = char.perform('attack');
 
   if (results !== undefined) {
@@ -95,4 +66,4 @@ test('performing an action not on character returns undefined', () => {
 
   const results = char.perform('attack');
   expect(results).toBeUndefined();
-})
+});
--- src/models/Character.ts
+++ src/models/Character.ts
@@ -1,37 +1,33 @@
-import { Action } from "./Action";
+import { Action } from './Action';
 
 export class Character {
   stats: Map<string, number> = new Map<string, number>();
   actions: Map<string, Action> = new Map<string, Action>();
 
-  constructor(
-    stats: [string, number][],
-    actions: [string, Action][]
-  ) {
+  constructor(stats: [string, number][], actions: [string, Action][]) {
     stats.forEach((cur) => {
-      var [statId, value] = cur;
+      const [statId, value] = cur;
       this.stats.set(statId, value);
     });
     actions.forEach((cur) => {
-      var [actionId, action] = cur;
+      const [actionId, action] = cur;
       this.actions.set(actionId, action);
     });
   }
 
-  public getStat(statId:string) {
+  public getStat(statId: string) {
     return this.stats.get(statId);
   }
 
-  public setStat(statId:string, value:number, 
-    overrideExistenceCheck:boolean=false) {
-      if (overrideExistenceCheck) {
-        this.stats.set(statId, value);
-      } else if (this.stats.has(statId)) {
-        this.stats.set(statId, value);
-      }
+  public setStat(statId: string, value: number, overrideExistenceCheck: boolean = false) {
+    if (overrideExistenceCheck) {
+      this.stats.set(statId, value);
+    } else if (this.stats.has(statId)) {
+      this.stats.set(statId, value);
+    }
   }
 
-  public perform(actionId:string) {
+  public perform(actionId: string) {
     return this.actions.get(actionId)?.take();
   }
-};
+}
